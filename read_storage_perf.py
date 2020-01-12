d8bcadb3b1f26c2e47e66d56d60abf86386d397c
Mustafa Kemal Mayuk <mkemalmayuk@gmail.com>
2020-01-12 15:54:56 +0300

    Create read_storage_perf.py

---
+++ read_storage_perf.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+ 
+from datetime import datetime
+import sys
+ 
+storage_name = sys.argv[1]
+pool_name = sys.argv[2]
+pool_dest_folder="/stor2rrd/ELK/" + storage_name + "/Pools/" + pool_name
+dest_file=pool_dest_folder + "/diff.log"
+ 
+with open(dest_file) as dfile:
+        content = dfile.readlines()
+content = [x.strip() for x in content]
+for line in content:
+        words = line.split()
+        ts = int(words[0].split(':')[0])
+        perf_time = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        perf_read = int(float(words[1].replace(',','.')))
+        perf_write = int(float(words[2].replace(',','.')))
+        perf_readio = int(float(words[3].replace(',','.')))
+        perf_writeio = int(float(words[4].replace(',','.')))
+        perf_respread = int(float(words[5].replace(',','.')))
+        perf_respwrite = int(float(words[6].replace(',','.')))
+        print "Storage:" + storage_name+ ",Pool:" + pool_name + ",Read:" + str(perf_read) + ",Write:" + str(perf_write) + ",ReadIO:" + str(perf_readio) + ",WriteIO:" + str(perf_writeio) + ",ReadResp:" + str(perf_respread) + ",WriteResp:" + str(perf_respwrite) + ",Time:" + str(perf_time)
